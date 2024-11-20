6655e6f0d26380fdbf96ad791b11dbbdd919df0f
zmomin13 <78432182+zmomin13@users.noreply.github.com>
2024-11-19 23:42:57 -0600

    Update __init__.py
    added import loginmanager back

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
